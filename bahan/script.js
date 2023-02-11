4dc3796af71ceb1667da185595d7fea10aba7cab
ZeroZennn <achmad.zikran@gmail.com>
2023-02-12 06:33:51 +0700

    assets eperpus

---
+++ bahan/script.js
@@ -0,0 +1,7 @@
+
+window.addEventListener("load", function(){
+  const loadScreen = document.querySelector(".loadScreen")
+  setTimeout(() => {
+    loadScreen.classList.add("loadScreen-hidden");
+  }, 5000)
+})
